fb44abb3b597dfd5dbf056a65c023e1d77a4c50f
cristianq3 <cristianq3@gmail.com>
2023-05-22 15:54:06 -0300

    Se agrega App y Componente Titulos

---
+++ ReactEjercicio2/src/components/Titulos.jsx
@@ -0,0 +1,11 @@
+const Titulos = ({nuevoTitulo}) => {  
+
+  return (
+    <section>
+      <h1>Primer App react</h1>
+      <h2>{nuevoTitulo}</h2>
+    </section>
+  );
+};
+
+export default Titulos;
